e8f229994324bf7da557cdd351d30a93f0231b0e
Elia Rice <eebrwriting@gmail.com>
2019-08-01 10:15:07 -0500

    Extra Commit

--- thatman17-project/src/main/java/com/revature/Login.java
+++ thatman17-project/src/main/java/com/revature/Login.java
@@ -16,6 +16,7 @@ public class Login extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
 
 public void doPost(HttpServletRequest request, HttpServletResponse response)  
         throws ServletException, IOException {  
