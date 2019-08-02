757ae4d55e11c1ec82f979ed679b02adcb996b57
Elia Rice <eebrwriting@gmail.com>
2019-08-02 00:37:58 -0500

    Beginning branch commit

--- thatman17-project/src/main/java/com/revature/servlets/Manager.java
+++ thatman17-project/src/main/java/com/revature/servlets/Manager.java
@@ -20,7 +20,22 @@ public class Manager extends HttpServlet {
 		HttpSession session = request.getSession(false);
 		if(session!=null) {
 			String name=(String)session.getAttribute("username");
-			out.print("Hello, "+name+" Welcome to Profile");  
+			out.println("<!DOCTYPE HTML>");
+			out.println("<html lang=\"en-us\">");
+			    out.println("<meta charset=\"UTF-8\">");
+			    out.println("<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">");
+			    out.println("<title>Management</title>");
+			    out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+			    out.println("</head>");
+			out.println("<body onload=\"getMovies()\">");
+			    out.println("<h1>Welcome, " + name + "</h1>");
+			    out.println("<div id=\"output\">");
+			        out.println("<h2>Employee Transactions</h2>");
+			        out.println("<div id=\"transactions\"></div>");
+			    out.println("</div>");
+			    out.println("<script src='main.js'></script>");
+			out.println("</body>");
+			out.println("</html>");
         }  
         else{  
             out.print("Please login first");  
