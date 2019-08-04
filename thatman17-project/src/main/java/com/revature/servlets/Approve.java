b89a2cb8b5c6ab97d9da2ba5249a1cc9aab47c3e
Elia Rice <eebrwriting@gmail.com>
2019-08-04 13:35:38 -0500

    Added Approval Function

--- thatman17-project/src/main/java/com/revature/servlets/Approve.java
+++ thatman17-project/src/main/java/com/revature/servlets/Approve.java
@@ -18,30 +18,36 @@ import com.revature.util.*;
  * Servlet implementation class Approve
  */
 public class Approve extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Approve() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	ConnectionUtil connectionUtil = new ConnectionUtil();
+    TransactionDao function = new TransactionDao(connectionUtil.getConnection());
+    
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter(); 
+		request.getRequestDispatcher("link.html").include(request, response);
+		HttpSession session = request.getSession(false);
+		if(session!=null) {
+		int id = Integer.parseInt(request.getParameter("id"));
+		int validate = 0;
+		String approval = request.getParameter("approval");
+		if (approval == null || id == 0) {
+			out.print("Please enter an id and hit approve or deny to approve or deny requests.");
+		} else if (approval.equals("Approve")) {
+			validate = 1;
+			function.approve(id, validate);
+			out.print("Request approved!");
+		} else if (approval.equals("Deny")) {
+			validate = 2;
+			function.approve(id, validate);
+			out.print("Request Denied!");
+		}
+		} else {
+			out.print("Sorry username or password error");  
+	        RequestDispatcher rd=request.getRequestDispatcher("index.html");  
+	        rd.include(request,response);  
+		}
 	}
 
 }
+
